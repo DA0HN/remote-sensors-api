949669153b9ad8eb53b6397e56d834f201b66529
Gabriel <gabrielhondacba@gmail.com>
2020-12-18 09:37:19 -0400

    feat: initial commit

---
+++ src/test/kotlin/org/gabriel/remotesensors/RemotesensorsApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.gabriel.remotesensors
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RemotesensorsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
